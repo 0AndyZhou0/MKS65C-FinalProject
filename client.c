0e81fd507b9fb741193913bc4b426169a3c56f31
Calvin Aw <caw@stuy.edu>
2019-01-07 21:56:10 -0500

    tf

--- client.c
+++ client.c
@@ -12,24 +12,26 @@ int main() {
   char receive[100];
   
   while(1){
-    while(read(from_server, receive, sizeof(receive)) > 0){
+    // while(read(from_server, receive, sizeof(receive)) > 0){
       //Receives Changed Information
       //from_server = client_handshake( &to_server );
-      read(from_server, receive, sizeof(receive));
+    
 
-      printf("Message received : %s\n", receive);
-    }
+    // }
     
     //Sending Information
     printf("%s\n", "input what you want to send");
     fgets(send, sizeof(send), stdin);
     strtok(send, "\n");
 
+
     if(send[0]){
       printf("send : %s\n",send);
       write(to_server, send, sizeof(send));
       send[0] = '\0';
     }
+    read(from_server, receive, sizeof(receive));
+    printf("Message received : %s\n", receive);
     
   }
   return 0;
