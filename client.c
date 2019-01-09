8282697d898606b0d2667cfd6a2d0d6feefcd2fc
Andy Zhou <azhou4@stuy.edu>
2019-01-09 15:37:04 -0500

    send help

--- client.c
+++ client.c
@@ -1,38 +1,21 @@
-#include "pipe_networking.h"
+#include "networking.h"
 
+int main(int argc, char **argv) {
 
-int main() {
+  int server_socket;
+  char buffer[BUFFER_SIZE];
 
-  int to_server;
-  int from_server;
+  if (argc == 2)
+    server_socket = client_setup( argv[1]);
+  else
+    server_socket = client_setup( TEST_IP );
 
-  from_server = client_handshake( &to_server );
-
-  char send[100];
-  char receive[100];
-  
-  while(1){
-    // while(read(from_server, receive, sizeof(receive)) > 0){
-      //Receives Changed Information
-      //from_server = client_handshake( &to_server );
-    
-
-    // }
-    
-    //Sending Information
-    printf("%s\n", "input what you want to send");
-    fgets(send, sizeof(send), stdin);
-    strtok(send, "\n");
-
-
-    if(send[0]){
-      printf("send : %s\n",send);
-      write(to_server, send, sizeof(send));
-      send[0] = '\0';
-    }
-    read(from_server, receive, sizeof(receive));
-    printf("Message received : %s\n", receive);
-    
+  while (1) {
+    printf(">");
+    fgets(buffer, sizeof(buffer), stdin);
+    *strchr(buffer, '\n') = 0;
+    write(server_socket, buffer, sizeof(buffer));
+    read(server_socket, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
   }
-  return 0;
 }
---
+++ forking_server.c
@@ -0,0 +1,68 @@
+#include "networking.h"
+
+//void process(char *s);
+void subserver(int from_client, int *from_clients,int i);
+
+int main() {
+
+  int listen_socket = server_setup();
+  int f;
+  int i = 0;
+  printf("--------------------\n");
+  //int listen_socket[10];
+  //for (; i<10; i++)
+  //listen_socket[i] = server_setup();
+  i = 0;
+  //int client_socket;
+  int client_socket[10];
+  printf("--------------------\n");
+
+  while (1) {
+    client_socket[i] = server_connect(listen_socket);
+    if(i < 10){
+      i++;
+      printf("%d\n",i);
+    }
+    
+    //f = fork();
+    if (fork() == 0){
+      for(int x = 0;x < i;x++){
+	subserver(client_socket[x], client_socket, i);
+      }
+    }
+    else{
+      for(int x = 0;x < i;x++){
+	close(client_socket[x]);
+      }
+    }
+  }
+
+  return 0;
+}
+
+void subserver(int client_socket, int *client_sockets, int i) {
+  char buffer[BUFFER_SIZE];
+  
+  while (read(client_socket, buffer, sizeof(buffer))) {
+    printf("hello");
+    printf("[subserver %d] received: [%s]\n", getpid(), buffer);
+    //process(buffer);
+    for(int x = 0;x < i;x++){
+      write(client_socket, buffer, sizeof(buffer));
+    }
+  }//end read loop
+  close(client_socket);
+  exit(0);
+}
+
+/*
+void process(char * s) {
+  while (*s) {
+    if (*s >= 'a' && *s <= 'z')
+      *s = ((*s - 'a') + 13) % 26 + 'a';
+    else  if (*s >= 'A' && *s <= 'Z')
+      *s = ((*s - 'a') + 13) % 26 + 'a';
+    s++;
+  }
+}
+*/
