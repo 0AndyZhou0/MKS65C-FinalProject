c12d3d2cfdb0cc1241ce47fd74c530d224aa59b9
Calvin Aw <caw@stuy.edu>
2019-01-10 01:11:57 -0500

    ok

--- client.c
+++ client.c
@@ -15,7 +15,11 @@ int main(int argc, char **argv) {
     fgets(buffer, sizeof(buffer), stdin);
     *strchr(buffer, '\n') = 0;
     write(server_socket, buffer, sizeof(buffer));
-    read(server_socket, buffer, sizeof(buffer));
+
+    if (fork() == 0){
+      read(server_socket, buffer, sizeof(buffer));
+
+    }
     printf("%s\n", buffer);
   }
 }
